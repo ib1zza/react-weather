1a4798773504f137c425643fe1d98b451a24be36
Mikhail Pshenichnyy <dremast1337@gmail.com>
2023-03-13 18:40:25 +0300

    removed link component from Footer.tsx

--- src/shared/Footer/Footer.tsx
+++ src/shared/Footer/Footer.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import s from "./Footer.module.scss";
-import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className={s.footer}>
       <p>Weather data by</p>
-      <Link to={"https://openweathermap.org/"}>openweathermap.org</Link>
+      <a href={"https://openweathermap.org/"} target={"_blank"}>
+        openweathermap.org
+      </a>
     </div>
   );
 };
